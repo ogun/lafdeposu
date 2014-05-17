e31c08e60716948e208426ae9832a73ff02490c2
Kemal Ogün Işık <ogunisik@hotmail.com>
2014-05-17 09:48:07 +0000

    Projede kullanılan kütüphaneler güncellendi
    git-tfs-id: [https://xxx.visualstudio.com/DefaultCollection/]$/LafDeposu;C57

--- src/LafDeposu.UI/App_Start/BundleConfig.cs
+++ src/LafDeposu.UI/App_Start/BundleConfig.cs
@@ -9,17 +9,17 @@ namespace LafDeposu.UI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/content/js/jquery-{version}.js"));
+                        "~/scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/index").Include(
                         "~/content/js/index/bottom.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/content/js/bootstrap.js"));
+                        "~/scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/content/js/angular.js",
-                        "~/content/js/angular-resource.js",
+                        "~/scripts/angular.js",
+                        "~/scripts/angular-resource.js",
                         "~/content/js/index/indexController.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/analytics").Include(
@@ -29,11 +29,11 @@ namespace LafDeposu.UI
                         "~/content/js/_layout/userreport.js"));
 
 
-            bundles.Add(new StyleBundle("~/content/bootstrap/")
-                        .Include("~/content/css/bootstrap.css", new CssRewriteUrlTransform())
+            bundles.Add(new StyleBundle("~/sbundles/bootstrap")
+                        .Include("~/content/bootstrap.css", new CssRewriteUrlTransform())
                         .Include("~/content/css/_layout/style.css"));
 
-            bundles.Add(new StyleBundle("~/content/index").Include(
+            bundles.Add(new StyleBundle("~/sbundles/index").Include(
                         "~/content/css/index/style.css"));
         }
     }
